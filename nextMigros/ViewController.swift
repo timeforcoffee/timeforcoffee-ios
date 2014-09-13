0e0af0774d5c6704c753d0e85c0b1f057cc5ab36
Christian Stocker <me@chregu.tv>
2014-09-13 09:20:43 +0200

    Initial Commit

---
+++ nextMigros/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nextMigros
+//
+//  Created by Christian Stocker on 13.09.14.
+//  Copyright (c) 2014 Christian Stocker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
