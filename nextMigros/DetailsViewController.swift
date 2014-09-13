3b2f0d9d740a5c78ffbfe6400772e85edc294667
Christian Stocker <me@chregu.tv>
2014-09-13 11:45:26 +0200

    initial code

---
+++ nextMigros/DetailsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DetailsViewController.swift
+//  nextMigros
+//
+//  Created by Christian Stocker on 13.09.14.
+//  Copyright (c) 2014 Christian Stocker. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+  
+    @IBOutlet weak var albumCover: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    var album: Album?
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = self.album?.title
+        albumCover.image = UIImage(data: NSData(contentsOfURL: NSURL(string: self.album!.largeImageURL)))
+    }
+}
---
+++ nextMigros/SearchResultsViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ViewController.swift
+//  nextMigros
+//
+//  Created by Christian Stocker on 13.09.14.
+//  Copyright (c) 2014 Christian Stocker. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, APIControllerProtocol {
+    @IBOutlet var appsTableView : UITableView?
+    var albums = [Album]()
+    let kCellIdentifier: String = "SearchResultCell"
+    var imageCache = [String : UIImage]()
+    var api : APIController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        api = APIController(delegate: self)
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        api?.searchItunesFor("Beatles")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return albums.count
+    }
+
+
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier) as UITableViewCell
+        
+        let album = self.albums[indexPath.row]
+        cell.textLabel?.text = album.title
+        cell.imageView?.image = UIImage(named: "Blank52")
+        
+        // Get the formatted price string for display in the subtitle
+        let formattedPrice = album.price
+        
+        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+        let urlString = album.thumbnailImageURL
+        
+        // Check our image cache for the existing key. This is just a dictionary of UIImages
+        //var image: UIImage? = self.imageCache.valueForKey(urlString) as? UIImage
+        var image = self.imageCache[urlString]
+        
+        
+        if( image == nil ) {
+            // If the image does not exist, we need to download it
+            var imgURL: NSURL = NSURL(string: urlString)
+            
+            // Download an NSData representation of the image at the URL
+            let request: NSURLRequest = NSURLRequest(URL: imgURL)
+            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: {(response: NSURLResponse!,data: NSData!,error: NSError!) -> Void in
+                if error == nil {
+                    image = UIImage(data: data)
+                    
+                    // Store the image in to our cache
+                    self.imageCache[urlString] = image
+                    if let cellToUpdate = tableView.cellForRowAtIndexPath(indexPath) {
+                        cellToUpdate.imageView?.image = image
+                    }
+                }
+                else {
+                    println("Error: \(error.localizedDescription)")
+                }
+            })
+            
+        }
+        else {
+            dispatch_async(dispatch_get_main_queue(), {
+                if let cellToUpdate = tableView.cellForRowAtIndexPath(indexPath) {
+                    cellToUpdate.imageView?.image = image
+                }
+            })
+        }
+        
+        cell.detailTextLabel?.text = formattedPrice
+        
+        return cell
+
+    }
+    
+    func didReceiveAPIResults(results: NSDictionary) {
+        var resultsArr: NSArray = results["results"] as NSArray
+        dispatch_async(dispatch_get_main_queue(), {
+            self.albums = Album.albumsWithJSON(resultsArr)
+            self.appsTableView!.reloadData()
+            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+        })
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        var detailsViewController: DetailsViewController = segue.destinationViewController as DetailsViewController
+        var albumIndex = appsTableView?.indexPathForSelectedRow()?.row
+//        var albumIndex = appsTableView!.indexPathForSelectedRow().row
+        var selectedAlbum = self.albums[albumIndex!]
+        detailsViewController.album = selectedAlbum
+    }
+
+    
+}
+
+
