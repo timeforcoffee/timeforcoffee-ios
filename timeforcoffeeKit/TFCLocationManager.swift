9b39695a3ef6437bbb5bed8f23518a8d22b16c35
Christian Stocker <me@chregu.tv>
2015-02-24 08:28:49 +0100

    start with TFCLocationManager class

---
+++ timeforcoffeeKit/TFCLocationManager.swift
@@ -0,0 +1,14 @@
+//
+//  LocationManager.swift
+//  timeforcoffee
+//
+//  Created by Christian Stocker on 24.02.15.
+//  Copyright (c) 2015 Christian Stocker. All rights reserved.
+//
+
+import Foundation
+
+public class TFCLocationManager {
+    
+    
+}
