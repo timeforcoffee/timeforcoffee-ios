63343be4d279931a412e9b2123d3a98c5aefe8b0
Christian Stocker <me@chregu.tv>
2015-02-24 13:40:46 +0100

    correct colors

--- timeforcoffee Widget/TodayViewController.swift
+++ timeforcoffee Widget/TodayViewController.swift
@@ -140,27 +140,14 @@ class TodayViewController: UIViewController, NCWidgetProviding, CLLocationManage
         lineNumberLabel.layer.cornerRadius = 4.0
         lineNumberLabel.layer.masksToBounds = true
         
-        switch departure.getLine() {
-        case "7":
-            lineNumberLabel.backgroundColor = UIColor.blackColor()
-            lineNumberLabel.textColor = UIColor.whiteColor()
-        case "11":
-            lineNumberLabel.backgroundColor = UIColor(red: 0/255, green: 137/255, blue: 47/255, alpha: 1)
-            lineNumberLabel.textColor = UIColor.whiteColor()
-        case "14":
-            lineNumberLabel.backgroundColor = UIColor(red: 0/255, green: 141/255, blue: 197/255, alpha: 1)
-            lineNumberLabel.textColor = UIColor.whiteColor()
-        case "15":
-            lineNumberLabel.backgroundColor = UIColor(red: 226/255, green: 10/255, blue: 22/255, alpha: 1)
-            lineNumberLabel.textColor = UIColor.whiteColor()
-        case "33":
-            lineNumberLabel.backgroundColor = UIColor(red: 218/255, green: 214/255, blue: 156/255, alpha: 1)
-            lineNumberLabel.textColor = UIColor.blackColor()
-        default:
+        if (departure.colorBg != nil) {
+            println("FOO")
+            println(departure.colorBg);
+            lineNumberLabel.backgroundColor = UIColor(netHexString:departure.colorBg!);
+            lineNumberLabel.textColor = UIColor(netHexString:departure.colorFg!);
+        } else {
             lineNumberLabel.textColor = UIColor.whiteColor()
         }
-        
-   
         
         return cell
         
@@ -210,4 +197,37 @@ class TodayViewController: UIViewController, NCWidgetProviding, CLLocationManage
         NSLog("Button Pressed")
     }
     
+}
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(netHex:Int) {
+        self.init(red:(netHex >> 16) & 0xff, green:(netHex >> 8) & 0xff, blue:netHex & 0xff)
+    }
+    
+    convenience init(netHexString: String) {
+        var cString:String = netHexString.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet() as NSCharacterSet).uppercaseString
+    
+        if (cString.hasPrefix("#")) {
+            cString = cString.substringFromIndex(advance(cString.startIndex, 1))
+        }
+        println(netHexString)
+        println(cString)
+        var rgbValue:UInt32 = 0
+        NSScanner(string: cString).scanHexInt(&rgbValue)
+    
+        self.init(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+            )
+    }
 }
