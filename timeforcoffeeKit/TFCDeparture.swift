772176cc39682e7710363291ccc6bfee18290213
gabac <gabathuler@gmail.com>
2015-03-05 20:36:55 +0100

    Fix colors.

--- timeforcoffeeKit/TFCDeparture.swift
+++ timeforcoffeeKit/TFCDeparture.swift
@@ -55,7 +55,11 @@ public class TFCDeparture {
                 var scheduledStr = result["departure"]["scheduled"].string
                 var realtimeStr = result["departure"]["realtime"].string
                 var colorFg = result["colors"]["fg"].string
+                colorFg = colorFg == nil ? "#000000" : colorFg
+
                 var colorBg = result["colors"]["bg"].string
+                colorBg = colorBg == nil ? "#ffffff" : colorBg
+                
                 var scheduled: NSDate?
                 var realtime: NSDate?
                 if (scheduledStr != nil) {
