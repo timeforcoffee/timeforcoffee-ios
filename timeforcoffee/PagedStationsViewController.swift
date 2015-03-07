99fdbb2027d018ff08df73e984e7d8b022f87b6f
Jan Hug <jan.hug@liip.ch>
2015-03-07 16:50:15 +0100

    WIP for the navbar titles

--- timeforcoffee/PagedStationsViewController.swift
+++ timeforcoffee/PagedStationsViewController.swift
@@ -9,11 +9,13 @@
 
 import Foundation
 
-class PagedStationsViewController: UIPageViewController, UIPageViewControllerDataSource {
+class PagedStationsViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate, UIScrollViewDelegate {
 
+    @IBOutlet weak var test: UINavigationItem!
 
     @IBOutlet var pageIndicator: UIPageControl!
-    var pageViewController: PagedStationsViewController?;
+    var pageViewController: PagedStationsViewController?
+    var currentPageIndex: Int?
 
 
     required init(coder aDecoder: NSCoder) {
@@ -23,6 +25,8 @@ class PagedStationsViewController: UIPageViewController, UIPageViewControllerDat
     override func viewDidLoad() {
         super.viewDidLoad()
         self.dataSource = self
+        self.delegate = self
+        currentPageIndex = 0
         var startingViewController = self.storyboard?.instantiateViewControllerWithIdentifier("StationsView") as StationsViewController
         startingViewController.showFavorites = false
         startingViewController.pageIndex = 0
@@ -35,15 +39,61 @@ class PagedStationsViewController: UIPageViewController, UIPageViewControllerDat
         let buttonAttr = [NSFontAttributeName: font]
         aboutButton.setTitleTextAttributes(buttonAttr, forState: UIControlState.Normal)
         self.navigationItem.rightBarButtonItem = aboutButton
+        
 
+        var titleView = UIView(frame: CGRect(x: 0, y: 0, width: 250, height: 30))
+        titleView.clipsToBounds = true
+        titleView.backgroundColor = UIColor.redColor()
+        
+        var labelContainer = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 30))
+        labelContainer.tag = 100
+        
+        var pageLabel1 = UILabel(frame: CGRect(x: 0, y: 0, width: 250, height: 30))
+        pageLabel1.text = "Nearest Stations"
+        pageLabel1.textAlignment = NSTextAlignment.Center
+        pageLabel1.backgroundColor = UIColor.blueColor()
+        
+        var pageLabel2 = UILabel(frame: CGRect(x: 250, y: 0, width: 250, height: 30))
+        pageLabel2.text = "Favorites"
+        pageLabel2.textAlignment = NSTextAlignment.Center
+        pageLabel2.backgroundColor = UIColor.greenColor()
+        
+        labelContainer.addSubview(pageLabel1)
+        labelContainer.addSubview(pageLabel2)
+        
+        titleView.addSubview(labelContainer)
+        
+        self.navigationItem.titleView = titleView
+    
+        
         let pageControl = UIPageControl.appearance()
-        pageControl.backgroundColor = UIColor.whiteColor()
+        pageControl.backgroundColor = UIColor.redColor()
         pageControl.pageIndicatorTintColor = UIColor.lightGrayColor()
         pageControl.currentPageIndicatorTintColor = UIColor.blackColor()
+        
+       
+        
+        for v in self.view.subviews {
+            if v.isKindOfClass(UIScrollView){
+                (v as UIScrollView).delegate = self
+            }
+        }
+        
     }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        
+        var realOffset = scrollView.contentOffset.x - scrollView.frame.width
+        
+        //self.navigationItem.titleView?.layer.position.x = scrollView.contentOffset.x
+        var xPosition = self.navigationItem.titleView?.viewWithTag(100)?.layer.position.x
+        self.navigationItem.titleView?.viewWithTag(100)?.layer.position.x = -(scrollView.contentOffset.x + (CGFloat(currentPageIndex!) * scrollView.frame.width))
+    }
+    
+    
+
 
     func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
-
         let vc:StationsViewController = viewController as StationsViewController
         if (vc.pageIndex == 1) {
             return nil;
@@ -64,6 +114,19 @@ class PagedStationsViewController: UIPageViewController, UIPageViewControllerDat
         newVc.showFavorites = false
         return newVc
     }
+    
+    func pageViewController(pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [AnyObject], transitionCompleted completed: Bool) {
+        if (completed) {
+            if (currentPageIndex == 0) {
+                currentPageIndex = 1
+            } else {
+                currentPageIndex = 0
+            }
+        }
+        println(completed)
+        println(currentPageIndex)
+    }
+    
 
     func aboutClicked(sender: UIBarButtonItem) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -76,5 +139,7 @@ class PagedStationsViewController: UIPageViewController, UIPageViewControllerDat
     func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
         return 0
     }
+    
+    
 
 }
