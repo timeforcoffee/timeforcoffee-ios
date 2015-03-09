5695c904ab810c63aa824994c56c178b26d1062d
Christian Stocker <me@chregu.tv>
2015-03-09 18:55:34 +0100

    Cache cacheable http requests

--- timeforcoffeeKit/APIController.swift
+++ timeforcoffeeKit/APIController.swift
@@ -13,21 +13,34 @@ public class APIController {
     
     var delegate: APIControllerProtocol
     var currentFetch: [Int: NSURLSessionDataTask] = [:]
-    
+    lazy var session:NSURLSession = {
+        return NSURLSession.sharedSession()
+    }()
+
+    lazy var cache:NSCache = {
+        println("new cache")
+        return NSCache()
+    }()
+
     public init(delegate: APIControllerProtocol) {
         self.delegate = delegate
     }
     
     public func searchFor(coord: CLLocationCoordinate2D) {
-        var urlPath = "http://transport.opendata.ch/v1/locations?x=\(coord.latitude)&y=\(coord.longitude)&type=station";
-        self.fetchUrl(urlPath, fetchId: 1)
+        var urlPath: String = String(format: "http://transport.opendata.ch/v1/locations?x=%.4f&y=%.4f", coord.latitude, coord.longitude)
+        if (cache.objectForKey(urlPath) != nil) {
+            let result = JSONValue(cache.objectForKey(urlPath) as NSData!);
+            self.delegate.didReceiveAPIResults(result, error: nil, context: nil)
+        } else {
+            self.fetchUrl(urlPath, fetchId: 1, cacheRequest: true)
+        }
     }
 
     public func searchFor(location: String) {
         let name = location.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())!
         let urlPath = "http://www.timeforcoffee.ch/api/zvv/stations/\(name)*";
 
-        self.fetchUrl(urlPath, fetchId: 1)
+        self.fetchUrl(urlPath, fetchId: 1, cacheRequest: true)
     }
     public func getDepartures(id: String!) {
         getDepartures(id, context: nil)
@@ -35,35 +48,51 @@ public class APIController {
     
     public func getDepartures(id: String!, context: Any?) {
         var urlPath = "http://www.timeforcoffee.ch/api/zvv/stationboard/\(id)"
-        self.fetchUrl(urlPath, fetchId: 2, context: context)
+        self.fetchUrl(urlPath, fetchId: 2, context: context, cacheRequest: false)
     }
     
-    func fetchUrl(urlPath: String, fetchId: Int) {
-        fetchUrl(urlPath, fetchId: fetchId, context: nil)
+    func fetchUrl(urlPath: String, fetchId: Int, cacheRequest: Bool) {
+        fetchUrl(urlPath, fetchId: fetchId, context: nil, cacheRequest: cacheRequest)
     }
 
-    func fetchUrl(urlPath: String, fetchId: Int, context: Any?) {
-        let urlPathEsc = urlPath.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
-        let url: NSURL = NSURL(string: urlPathEsc)!
-        let session = NSURLSession.sharedSession()
-        println("Start fetching data \(urlPath)")
-        if (currentFetch[fetchId] != nil) {
-            currentFetch[fetchId]?.cancel()
-        }
-        currentFetch[fetchId] = session.dataTaskWithURL(url, completionHandler: {data , response, error -> Void in
-            println("Task completed")
-            if(error != nil) {
-                // If there is an error in the web request, print it to the console
-                println(error.localizedDescription)
+    func fetchUrl(urlPath: String, fetchId: Int, context: Any?, cacheRequest: Bool) {
+        if (cacheRequest && cache.objectForKey(urlPath) != nil) {
+            let result = JSONValue(cache.objectForKey(urlPath) as NSData!);
+            self.delegate.didReceiveAPIResults(result, error: nil, context: context)
+        } else {
+            let urlPathEsc = urlPath.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+            let url: NSURL = NSURL(string: urlPathEsc)!
+            println("Start fetching data \(urlPath)")
+            if (currentFetch[fetchId] != nil) {
+                currentFetch[fetchId]?.cancel()
             }
-            self.currentFetch[fetchId] = nil
-            var err: NSError?
-            let jsonResult = JSONValue(data)
-            self.delegate.didReceiveAPIResults(jsonResult, error: error, context: context)
-        })
-        
-        currentFetch[fetchId]?.resume()
+            let session2 = self.session
+            let request = NSURLRequest(URL: url, cachePolicy: NSURLRequestCachePolicy.ReturnCacheDataElseLoad, timeoutInterval: 10.0)
 
+
+            currentFetch[fetchId] = session2.dataTaskWithRequest(request, completionHandler: {data , response, error -> Void in
+
+                println("Task completed")
+                if(error != nil) {
+                    // If there is an error in the web request, print it to the console
+                    println(error.localizedDescription)
+                }
+                self.currentFetch[fetchId] = nil
+                var err: NSError?
+                let jsonResult = JSONValue(data)
+                if (error == nil && cacheRequest) {
+                    let HTTPResponse: NSHTTPURLResponse = response as NSHTTPURLResponse
+                    let url: String? = HTTPResponse.URL?.absoluteString
+                    println("CACHE")
+                    if (url != nil) {
+                        self.cache.setObject(data, forKey: url!)
+                    }
+                }
+                self.delegate.didReceiveAPIResults(jsonResult, error: error, context: context)
+            })
+            
+            currentFetch[fetchId]?.resume()
+        }
     }
 }
 
