cddfd2d5e312abe059111290eb684baf86be95c1
Jan Hug <jan.hug@liip.ch>
2015-03-13 22:08:03 +0100

    Create DepartureLineView class

---
+++ timeforcoffeeKit/DepartureLineView.swift
@@ -0,0 +1,19 @@
+//
+//  DepartureLineView.swift
+//  timeforcoffee
+//
+//  Created by Jan Hug on 13.03.15.
+//  Copyright (c) 2015 Christian Stocker. All rights reserved.
+//
+
+import UIKit
+
+class DepartureLineView: UIView {
+    
+    var test: String = "jaaaaa"
+    
+    func setLine(line: String) {
+        
+    }
+    
+}
