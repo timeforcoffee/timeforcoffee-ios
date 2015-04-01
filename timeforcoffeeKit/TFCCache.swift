ccc5a163bd92a50a9b878a4b9575bc4dd7fdf9f5
Christian Stocker <me@chregu.tv>
2015-04-01 17:17:03 +0200

    return a non-shared cache, when provisioning profiles are missing

--- timeforcoffeeKit/TFCCache.swift
+++ timeforcoffeeKit/TFCCache.swift
@@ -12,14 +12,14 @@ import PINCache
 class TFCCache {
     struct objects {
         static var apicalls: PINCache = {
-            var p = PINCache(name: "apicalls", rootPath: TFCCache.getRootDirectory())
+            var p = TFCCache.getCacheInstance("apicalls")
             // cache for max 7 days
             p.diskCache.ageLimit = 60 * 60 * 24 * 7 // 7 days
             p.diskCache.byteLimit = 5 * 1024 * 1024 // 5 MB
             return p
             }()
         static var stations: PINCache = {
-            var p = PINCache(name: "stations", rootPath: TFCCache.getRootDirectory())
+            var p = TFCCache.getCacheInstance("stations")
             // cache for max 24 hours
             p.diskCache.ageLimit = 60 * 60 * 24 // 1 day
             p.diskCache.byteLimit = 5 * 1024 * 1024 // 5 MB
@@ -28,9 +28,19 @@ class TFCCache {
         }()
     }
 
-    class func getRootDirectory() -> String? {
+    private class func getRootDirectory() -> String? {
         let manager = NSFileManager.defaultManager()
         let documentsDirectory2 = manager.containerURLForSecurityApplicationGroupIdentifier("group.ch.liip.timeforcoffee")
         return (documentsDirectory2?.path)
+    }
+
+    private class func getCacheInstance(name: String) -> PINCache {
+        let rootDir = getRootDirectory()
+        // to make sure that cache still works, even if we can't get a shared directory
+        //  the cache won't be shared then between app and extensions, but it still works
+        if (rootDir == nil) {
+            return PINCache(name: name)
+        }
+        return PINCache(name: "apicalls", rootPath: rootDir)
     }
 }
