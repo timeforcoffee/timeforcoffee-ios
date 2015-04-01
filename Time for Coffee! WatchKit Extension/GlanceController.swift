cb02a94ddd7b07b0baefbd0282a995a9cd6a639f
gabac <gabathuler@gmail.com>
2015-04-01 17:07:13 +0200

    Connect Interfacebuilder with code.

--- Time for Coffee! WatchKit Extension/GlanceController.swift
+++ Time for Coffee! WatchKit Extension/GlanceController.swift
@@ -13,14 +13,28 @@ import Foundation
 class GlanceController: WKInterfaceController {
 
     @IBOutlet weak var minutesLabel: WKInterfaceLabel!
+    @IBOutlet weak var destinationLabel: WKInterfaceLabel!
+    @IBOutlet weak var numberGroup: WKInterfaceGroup!
+    @IBOutlet weak var depatureLabel: WKInterfaceLabel!
+    @IBOutlet weak var numberLabel: WKInterfaceLabel!
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
         // Configure interface objects here.
     }
 
+
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
+        
+        minutesLabel.setText("6'")
+        destinationLabel.setText("Röntgenstrasse");
+        depatureLabel.setText("In 6' / 16:59");
+        numberLabel.setText("12")
+        numberLabel.setTextColor(UIColor.whiteColor())
+        numberGroup.setBackgroundColor(UIColor.greenColor())
+        
         super.willActivate()
     }
 
