4edb8608f6dde614a986430094466a3ca63e58e9
Christian Stocker <me@chregu.tv>
2015-04-16 10:39:31 +0200

    disable map rotation

--- timeforcoffee/DeparturesViewController.swift
+++ timeforcoffee/DeparturesViewController.swift
@@ -123,6 +123,7 @@ final class DeparturesViewController: UIViewController, UITableViewDataSource, U
 
         topViewProperties(0.0)
         self.mapView?.userInteractionEnabled = false;
+        self.mapView?.rotateEnabled = false
         var region = MKCoordinateRegionMakeWithDistance((station?.coord?.coordinate)! ,450,450);
         self.mapView.setRegion(region, animated: false)
         // put it to true when within a few hundred meters
