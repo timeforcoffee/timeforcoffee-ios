607e19aeaa2fde6d2dcc85328cbf923a57e32337
Christian Stocker <me@chregu.tv>
2015-05-22 20:48:00 +0200

    show 3 departures in the glance

--- Time for Coffee! WatchKit Extension/GlanceController.swift
+++ Time for Coffee! WatchKit Extension/GlanceController.swift
@@ -37,84 +37,64 @@ class GlanceController: WKInterfaceController {
             for (station) in stations! {
                 station.updateDepartures(nil)
                 let departures = station.getFilteredDepartures()
-                var dobreak = false
                 if(departures?.count > 0) {
-                    //todo only ask for one depature
-                    if let firstDepature = departures?[0] {
-                        let to = firstDepature.getDestination(station)
+                    var i = 0
+                    stationLabel.setText(station.getName(true))
+                    stationsTable.setNumberOfRows(min(3,(departures?.count)!), withRowType: "station")
+                    for (secondDepature) in departures! {
+                        if let sr = stationsTable.rowControllerAtIndex(i) as! StationRow? {
+                            let to = secondDepature.getDestination(station)
+                            let name = secondDepature.getLine()                // doesn't work yet  with the font;(
+                            let helvetica = UIFont(name: "HelveticaNeue-Bold", size: 18.0)!
+                            var fontAttrs = [NSFontAttributeName : helvetica]
+                            var attrString = NSAttributedString(string: name, attributes: fontAttrs)
+                            if let numberLabel = sr.numberLabel {
+                                numberLabel.setAttributedText(attrString)
+                            }
+                            if let label = sr.destinationLabel {
+                                label.setText(to)
+                            }
+                            if let label = sr.depatureLabel {
+                                let (departureTimeAttr, departureTimeString) = secondDepature.getDepartureTime()
+                                if (departureTimeAttr != nil) {
+                                    label.setAttributedText(departureTimeAttr)
+                                } else {
+                                    label.setText(departureTimeString)
+                                }
+                            }
+                            if let label = sr.minutesLabel {
+                                label.setText(secondDepature.getMinutes())
+                            }
+                            if (secondDepature.colorBg != nil) {
+                                if let group = sr.numberGroup {
+                                    group.setBackgroundColor(UIColor(netHexString:(secondDepature.colorBg)!))
+                                }
+                                if let label = sr.numberLabel {
+                                    label.setTextColor(UIColor(netHexString:(secondDepature.colorFg)!))
+                                }
+                            }
+                            i++
+                            if (i >= 3) {
+                                break;
+                            }
 
-                        minutesLabel.setText("in \(firstDepature.getMinutes()!)")
-                        destinationLabel.setText("To \(to)");
-                        stationLabel.setText(station.getName(true))
-                        let (departureTimeAttr, departureTimeString) = firstDepature.getDepartureTime()
-                        if (departureTimeAttr != nil) {
-                            departureLabel.setAttributedText(departureTimeAttr)
-                        } else {
-                            departureLabel.setText(departureTimeString)
-                        }
-                        numberLabel.setText(firstDepature.getLine())
-                        numberLabel.setTextColor(UIColor(netHexString:(firstDepature.colorFg)!))
-                        numberGroup.setBackgroundColor(UIColor(netHexString:(firstDepature.colorBg)!))
-                        println("\(to)")
-                        dobreak = true
-                    }
-
-                    if(departures?.count > 1) {
-
-                        if let secondDepature = departures?[1] {
-                            stationsTable.setNumberOfRows(1, withRowType: "station")
-                           // for (deptstation) in departures2 {
-                                if let sr = stationsTable.rowControllerAtIndex(0) as! StationRow? {
-                                    let to = secondDepature.getDestination(station)
-                                    let name = secondDepature.getLine()                // doesn't work yet  with the font;(
-                                    let helvetica = UIFont(name: "HelveticaNeue-Bold", size: 18.0)!
-                                    var fontAttrs = [NSFontAttributeName : helvetica]
-                                    var attrString = NSAttributedString(string: name, attributes: fontAttrs)
-                                    if let numberLabel = sr.numberLabel {
-                                        numberLabel.setAttributedText(attrString)
-                                    }
-                                    if let label = sr.destinationLabel {
-                                        label.setText(to)
-                                    }
-                                    if let label = sr.depatureLabel {
-                                        let (departureTimeAttr, departureTimeString) = secondDepature.getDepartureTime()
-                                        if (departureTimeAttr != nil) {
-                                            label.setAttributedText(departureTimeAttr)
-                                        } else {
-                                            label.setText(departureTimeString)
-                                        }
-                                    }
-                                    if let label = sr.minutesLabel {
-                                        label.setText(secondDepature.getMinutes())
-                                    }
-                                    if (secondDepature.colorBg != nil) {
-                                        if let group = sr.numberGroup {
-                                            group.setBackgroundColor(UIColor(netHexString:(secondDepature.colorBg)!))
-                                        }
-                                        if let label = sr.numberLabel {
-                                            label.setTextColor(UIColor(netHexString:(secondDepature.colorFg)!))
-                                        }
-                                    }
-                                    
-                                }                          
-                          //  }
                         }
                     }
-                    if (dobreak) {
-                        break;
-                    }
+                    break;
                 }
             }
         }
         TFCWatchData.sharedInstance.getStations(handleReply, stopWithFavorites: true)
 
-        minutesLabel.setText("")
         stationLabel.setText("Loading ...");
+        //stationsTable.setNumberOfRows(0, withRowType: "station")
+
+     /*   minutesLabel.setText("")
         destinationLabel.setText("")
         departureLabel.setText("");
         numberLabel.setText("")
         numberLabel.setTextColor(UIColor.whiteColor())
-        numberGroup.setBackgroundColor(UIColor.clearColor())
+        numberGroup.setBackgroundColor(UIColor.clearColor())*/
         
     }
 
