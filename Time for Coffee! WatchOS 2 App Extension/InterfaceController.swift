c989f5d0ef80c1bdcbb220724827e67bc4490829
Christian Stocker <me@chregu.tv>
2015-08-22 00:31:34 +0200

    clean upgrade from xcode 6.4 for watchos2

---
+++ Time for Coffee! WatchOS 2 App Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Time for Coffee! WatchOS 2 App Extension
+//
+//  Created by Christian Stocker on 22.08.15.
+//  Copyright © 2015 Christian Stocker. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ timeforcoffeeWatchKit/timeforcoffeeWatchKit.h
@@ -0,0 +1,19 @@
+//
+//  timeforcoffeeWatchKit.h
+//  timeforcoffeeWatchKit
+//
+//  Created by Christian Stocker on 22.08.15.
+//  Copyright © 2015 Christian Stocker. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for timeforcoffeeWatchKit.
+FOUNDATION_EXPORT double timeforcoffeeWatchKitVersionNumber;
+
+//! Project version string for timeforcoffeeWatchKit.
+FOUNDATION_EXPORT const unsigned char timeforcoffeeWatchKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <timeforcoffeeWatchKit/PublicHeader.h>
+
+
