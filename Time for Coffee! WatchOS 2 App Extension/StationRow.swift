1ed4296cf68382d5e95b5c6bdb3abfd6ffd1fc6b
Christian Stocker <me@chregu.tv>
2015-09-12 18:35:02 +0200

    don't cache row updates

--- Time for Coffee! WatchOS 2 App Extension/StationRow.swift
+++ Time for Coffee! WatchOS 2 App Extension/StationRow.swift
@@ -22,68 +22,39 @@ class StationRow: NSObject {
 
     @IBOutlet weak var numberGroup: WKInterfaceGroup!
 
-    var to: String?
-    var name: String?
-    var departureTime: String?
-    var minutes: String?
-    var plannedDepartureTime: String?
-
     func drawCell(departure: TFCDeparture, station: TFCStation ) {
         let to = departure.getDestination(station)
         let name = departure.getLine()
-        var redrawAll = false
-        let plannedDepartureTime = departure.getScheduledTime()
-        if (plannedDepartureTime != self.plannedDepartureTime) {
-            self.plannedDepartureTime = plannedDepartureTime
-            redrawAll = true
+        /* let helvetica = UIFont(name: "HelveticaNeue-Bold", size: 18.0)!
+        var fontAttrs = [NSFontAttributeName : helvetica]
+        var attrString = NSAttributedString(string: name, attributes: fontAttrs)*/
+        if (departure.colorBg != nil) {
+            if let group = self.numberGroup {
+                group.setBackgroundColor(UIColor(netHexString:(departure.colorBg)!))
+            }
+            if let label = self.numberLabel {
+                label.setTextColor(UIColor(netHexString:(departure.colorFg)!))
+            }
         }
-        if (redrawAll || name != self.name) {
-            /* let helvetica = UIFont(name: "HelveticaNeue-Bold", size: 18.0)!
-            var fontAttrs = [NSFontAttributeName : helvetica]
-            var attrString = NSAttributedString(string: name, attributes: fontAttrs)*/
-            if (departure.colorBg != nil) {
-                if let group = self.numberGroup {
-                    group.setBackgroundColor(UIColor(netHexString:(departure.colorBg)!))
-                }
-                if let label = self.numberLabel {
-                    label.setTextColor(UIColor(netHexString:(departure.colorFg)!))
-                }
-            }
-            if let numberLabel = self.numberLabel {
-                numberLabel.setText(name)
-            }
-            self.name = name
+        if let numberLabel = self.numberLabel {
+            numberLabel.setText(name)
         }
-        if (redrawAll || self.to != to) {
-            if let label = self.destinationLabel {
-                label.setText(to)
-            }
-            self.to = to
+
+        if let label = self.destinationLabel {
+            label.setText(to)
         }
 
         let (departureTimeAttr, departureTimeString) = departure.getDepartureTime(false)
-        var departureTime: String?
-        if (departureTimeAttr != nil) {
-            departureTime = departureTimeAttr?.string
-        } else {
-            departureTime = departureTimeString
-        }
-        if (redrawAll ||  departureTime != self.departureTime) {
-            if let label = self.depatureLabel {
-                if (departureTimeAttr != nil) {
-                    label.setAttributedText(departureTimeAttr)
-                } else {
-                    label.setText(departureTimeString)
-                }
+        if let label = self.depatureLabel {
+            if (departureTimeAttr != nil) {
+                label.setAttributedText(departureTimeAttr)
+            } else {
+                label.setText(departureTimeString)
             }
-            self.departureTime = departureTime
         }
         let minutes = departure.getMinutes()
-        if (redrawAll || minutes != self.minutes) {
-            if let label = self.minutesLabel {
-                label.setText(minutes)
-            }
-            self.minutes = minutes
+        if let label = self.minutesLabel {
+            label.setText(minutes)
         }
         if (topGroup != nil) {
             topGroup.setHidden(false)
