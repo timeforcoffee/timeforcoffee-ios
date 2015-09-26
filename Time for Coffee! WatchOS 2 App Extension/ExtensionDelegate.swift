b3c92349ca679ab6108bfe1b23cdd9981578aac0
Christian Stocker <me@chregu.tv>
2015-09-26 09:02:37 +0200

    watch app runs!

--- Time for Coffee! WatchOS 2 App Extension/ExtensionDelegate.swift
+++ Time for Coffee! WatchOS 2 App Extension/ExtensionDelegate.swift
@@ -2,25 +2,53 @@
 //  ExtensionDelegate.swift
 //  Time for Coffee! WatchOS 2 App Extension
 //
-//  Created by Christian Stocker on 25.09.15.
-//  Copyright © 2015 opendata.ch. All rights reserved.
+//  Created by Christian Stocker on 11.09.15.
+//  Copyright © 2015 Christian Stocker. All rights reserved.
 //
 
 import WatchKit
+import timeforcoffeeWatchKit
 
 class ExtensionDelegate: NSObject, WKExtensionDelegate {
 
     func applicationDidFinishLaunching() {
-        // Perform any final initialization of your application.
+
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0)) {
+            TFCDataStore.sharedInstance.registerWatchConnectivity()
+            /* Request for all Favorite Data every 24 hours (or if never done)
+                I'm not sure, how reliable the WatchConnectivity is and if never
+                gets a message lost, so let's sync every 24 hours. Shouldn't be
+                that much data anyway
+            
+                Or if we never did get a allDataResponse, do it every time until we get one.
+            */
+            let lastRequest = self.lastRequestForAllData()
+            let allDataResponseSent = TFCDataStore.sharedInstance.getUserDefaults()?.boolForKey("allDataResponseSent")
+            if (allDataResponseSent != true || lastRequest == nil || lastRequest < -(24 * 60 * 60)) {
+                TFCDataStore.sharedInstance.requestAllDataFromPhone()
+                TFCDataStore.sharedInstance.getUserDefaults()?.setObject(NSDate(), forKey: "lastRequestForAllData")
+            }
+        }
+
+        // iCLoud not supported (yet?) by watchOS :(
+    /*    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0)) {
+            TFCDataStore.sharedInstance.registerForNotifications()
+            TFCDataStore.sharedInstance.synchronize()
+        }*/
     }
 
     func applicationDidBecomeActive() {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
     func applicationWillResignActive() {
+
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, etc.
     }
 
+    private func lastRequestForAllData() -> NSTimeInterval? {
+        let lastUpdate: NSDate? = TFCDataStore.sharedInstance.getUserDefaults()?.objectForKey("lastRequestForAllData") as! NSDate?
+        return lastUpdate?.timeIntervalSinceNow
+    }
 }
+
---
+++ Time for Coffee! WatchOS 2 App Extension/StationRow.swift
@@ -0,0 +1,67 @@
+//
+//  StationRow.swift
+//  timeforcoffee
+//
+//  Created by Christian Stocker on 02.03.15.
+//  Copyright (c) 2015 Christian Stocker. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+import timeforcoffeeWatchKit
+
+class StationRow: NSObject {
+
+    @IBOutlet weak var destinationLabel: WKInterfaceLabel!
+    @IBOutlet weak var depatureLabel: WKInterfaceLabel!
+    @IBOutlet weak var minutesLabel: WKInterfaceLabel!
+
+    @IBOutlet weak var numberLabel: WKInterfaceLabel!
+
+    @IBOutlet weak var topGroup: WKInterfaceGroup!
+
+    @IBOutlet weak var numberGroup: WKInterfaceGroup!
+
+    func drawCell(departure: TFCDeparture, station: TFCStation ) {
+        let to = departure.getDestination(station)
+        let name = departure.getLine()
+        /* let helvetica = UIFont(name: "HelveticaNeue-Bold", size: 18.0)!
+        var fontAttrs = [NSFontAttributeName : helvetica]
+        var attrString = NSAttributedString(string: name, attributes: fontAttrs)*/
+        if (departure.colorBg != nil) {
+            if let group = self.numberGroup {
+                group.setBackgroundColor(UIColor(netHexString:(departure.colorBg)!))
+            }
+            if let label = self.numberLabel {
+                label.setTextColor(UIColor(netHexString:(departure.colorFg)!))
+            }
+        }
+        if let numberLabel = self.numberLabel {
+            numberLabel.setText(name)
+        }
+
+        if let label = self.destinationLabel {
+            label.setText(to)
+        }
+
+        let (departureTimeAttr, departureTimeString) = departure.getDepartureTime(false)
+        if let label = self.depatureLabel {
+            if (departureTimeAttr != nil) {
+                label.setAttributedText(departureTimeAttr)
+            } else {
+                label.setText(departureTimeString)
+            }
+        }
+        let minutes = departure.getMinutes()
+        if let label = self.minutesLabel {
+            label.setText(minutes)
+        }
+        if (topGroup != nil) {
+            topGroup.setHidden(false)
+        }
+    }
+
+    func setHidden() {
+        topGroup.setHidden(true)
+    }
+}
