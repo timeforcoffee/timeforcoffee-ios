fb276db64537511669a0d85a8cf27cc528aafb2b
Christian Stocker <me@chregu.tv>
2016-03-17 23:09:41 +0100

    sync datastore on app background

--- timeforcoffee/AppDelegate.swift
+++ timeforcoffee/AppDelegate.swift
@@ -168,6 +168,9 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+
+        TFCDataStore.sharedInstance.synchronize()
+
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
