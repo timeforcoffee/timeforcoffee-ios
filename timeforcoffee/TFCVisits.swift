1c5a30a8213e68fbd6084e8a50aa8f7f22e9a5ff
Christian Stocker <me@chregu.tv>
2016-09-26 17:18:47 +0200

    don't update complication when not favorite  for region visit

--- timeforcoffee/TFCVisits.swift
+++ timeforcoffee/TFCVisits.swift
@@ -17,7 +17,10 @@ class TFCVisits: NSObject, TFCLocationManagerDelegate, TFCStationsUpdatedProtoco
     lazy var stations: TFCStations = {return TFCStations(delegate: self)}()
     var callback: ((text: String) -> ())? = nil
 
-    var geofenceUpdated = false
+    private var lastWasRegionVisit = false
+
+    private var geofenceUpdated = false
+
     override init() {
 
         super.init()
@@ -72,6 +75,7 @@ class TFCVisits: NSObject, TFCLocationManagerDelegate, TFCStationsUpdatedProtoco
             TFCFavorites.sharedInstance.updateGeofences(force: false)
             return
         }
+        lastWasRegionVisit = true
         let station = TFCStation.initWithCacheId(region.identifier)
         DLog("visited fence for \(station.name)", toFile: true)
         DLog("fence: currentLocation: \(locManager?.currentLocation)")
@@ -84,10 +88,24 @@ class TFCVisits: NSObject, TFCLocationManagerDelegate, TFCStationsUpdatedProtoco
         if (favoritesOnly == false) { // wait for all stations, should be fast anyway with the DB lookup nowadays (in Switzerland at least) and doesn't matter in this case how fast it is
             DLog("TFCVisits stationsUpdate")
             if let station = self.stations.first {
+                var doComplicationUpdate = true
+                // don't update complication, if the first staion is not a favorite and we're coming from a region update
+                // this can happen when we hardly touch a region perimeter but are out of it again when we have the first station
+                // the implemented code can lead to false assumptions (since several events can happen at one time), but this would only
+                // lead to do a non favorite complication update which shouldn't be done for optimization reasons and not the other way
+                // round. We can live with that
+                if (lastWasRegionVisit && !station.isFavorite()) {
+                    doComplicationUpdate = false
+                    DLog("region visit but nearest station \(station.name) is not a favorite. not sending complication update", toFile: true)
+                }
                 DLog("first station is \(station)", toFile: true)
                 TFCDataStore.sharedInstance.sendComplicationUpdate(station, coord: TFCLocationManagerBase.getCurrentLocation()?.coordinate)
+                if (doComplicationUpdate) {
+                    TFCDataStore.sharedInstance.sendComplicationUpdate(station, coord: TFCLocationManagerBase.getCurrentLocation()?.coordinate)
+                }
             }
         }
+        self.lastWasRegionVisit = false
     }
 }
 
