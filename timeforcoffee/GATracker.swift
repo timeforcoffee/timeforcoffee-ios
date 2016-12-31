921d54ce9e038b789122634a1d4742de73ad4a5b
Christian Stocker <me@chregu.tv>
2016-12-31 09:24:54 +0100

    make it possible to disable GAtracker

--- timeforcoffee/GATracker.swift
+++ timeforcoffee/GATracker.swift
@@ -10,39 +10,49 @@ import Foundation
 
 class GATracker {
 
-    class var sharedInstance: GATracker? {
+    /*class var sharedInstance: GATracker? {
         struct Static {
-            static let instance: GATracker = GATracker()
+            static var instance: GATracker = GATracker()
         }
+     //   return nil
         return Static.instance
-    }
+    }*/
 
+    static var sharedInstance:GATracker? = GATracker()
 
-    let gtracker = GAI.sharedInstance()
+    var gtracker:GAI? = nil
     var toBeSentCustomDimensions:[String:String] = [:]
 
     init() {
-        gtracker.trackUncaughtExceptions = true
-        gtracker.dispatchInterval = 20;
+        gtracker = GAI.sharedInstance()
+        gtracker?.trackUncaughtExceptions = true
+        gtracker?.dispatchInterval = 30;
         //GAI.sharedInstance().logger.logLevel = GAILogLevel.Verbose
-        gtracker.trackerWithTrackingId("UA-37092982-2")
+        gtracker?.trackerWithTrackingId("UA-37092982-2")
     }
+
+    func deinitTracker() {
+        gtracker?.dispatchInterval = -1;
+        gtracker = nil
+        GATracker.sharedInstance = nil
+    }
+
     func setCustomDimension(index:UInt, value:String) {
         let field = GAIFields.customDimensionForIndex(index)
-        gtracker.defaultTracker.set(field, value: value)
+        gtracker?.defaultTracker.set(field, value: value)
         toBeSentCustomDimensions[field] = value
     }
 
     func sendScreenName(name:String) {
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0)) {
-            self.gtracker.defaultTracker.set(kGAIScreenName, value: name)
+            self.gtracker?.defaultTracker.set(kGAIScreenName, value: name)
             var s = GAIDictionaryBuilder.createScreenView()
             for (key, value) in self.toBeSentCustomDimensions {
                 s = s.set(value, forKey: key)
             }
             self.toBeSentCustomDimensions.removeAll()
             DLog("\(s.build() as [NSObject : AnyObject])")
-            self.gtracker.defaultTracker.send(s.build() as [NSObject : AnyObject]!)
+            self.gtracker?.defaultTracker.send(s.build() as [NSObject : AnyObject]!)
         }
 
     }
