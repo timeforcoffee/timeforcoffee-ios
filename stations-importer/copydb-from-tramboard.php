119a81cf60ad52624e20f018ccc552dc60f5a08c
Christian Stocker <me@chregu.tv>
2017-01-12 21:13:15 +0100

    drop unneeded indexes

--- stations-importer/copydb-from-tramboard.php
+++ stations-importer/copydb-from-tramboard.php
@@ -4,10 +4,11 @@ $handle = new SQLite3("../../tramboard-clj/stations.sqlite");
 $handle2 = new SQLite3("../timeforcoffeeKit/SingleViewCoreData.sqlite");
 $results = $handle->query("select Z_PK, Z_ENT, Z_OPT, ZLASTUPDATED, ZLATITUDE, ZLONGITUDE, ZCOUNTRYISO, ZID, ZNAME from ZTFCSTATIONMODEL ");
 $handle2->exec("drop TABLE zvv_to_sbb");
+$handle2->exec("drop index ZAPIID");
+$handle2->exec("drop index ZAPIKEY");
 
 $handle2->exec("delete from ZTFCSTATIONMODEL");
 $handle2->exec("delete from ZTFCDEPARTURE");
-
 while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
 
     
