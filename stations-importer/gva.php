bec632e71835bf35a46a48bb07bdcf77467c5c74
Christian Stocker <me@chregu.tv>
2017-10-21 10:33:22 +0200

    fix gva importer

--- stations-importer/gva.php
+++ stations-importer/gva.php
@@ -1,7 +1,7 @@
 <?php
 
-$handle = new SQLite3("/Users/chregu/Documents/SingleViewCoreData.sqlite");
-$results = $handle->query("select * from ZTFCSTATIONMODEL where ZCOUNTY = 'Geneva' AND ZDEPARTURESURL ISNULL  "); 
+$handle = new SQLite3("../../tramboard-clj/stations.sqlite");
+$results = $handle->query("select * from ZTFCSTATIONMODEL where ZCOUNTY = 'Geneva' AND ZAPIKEY ISNULL  ");
 
 while ($row = $results->fetchArray()) {
     $url = "http://prod.ivtr-od.tpg.ch/v1/GetStops.json?latitude=" . $row['ZLATITUDE'] . "&longitude=" . $row['ZLONGITUDE'] . "&key=21a25080-9bbc-11e4-bc99-0002a5d5c51b";
@@ -15,17 +15,29 @@ while ($row = $results->fetchArray()) {
         print " ". $stop['stopName'];
         print "\n";
         if ($distance < 50) {
-            $handle->exec("update ZTFCSTATIONMODEL set ZAPIKEY = 'gva', ZAPIID = '". $stop['stopCode'] . "' where ZID = '". $row['ZID'] ."'");
+            if (checkData($stop['stopCode'])) {
+                $handle->exec("update ZTFCSTATIONMODEL set ZAPIKEY = 'gva', ZAPIID = '". $stop['stopCode'] . "' where ZID = '". $row['ZID'] ."'");
+            } else {
+                print "No departures found for " . $row['ZNAME'] . "\n";
+            }
         } if ($distance < 250) {
             if ($row['ZNAME'] ==  $stop['stopName']) {
-                $handle->exec("update ZTFCSTATIONMODEL set ZAPIKEY = 'gva', ZAPIID = '". $stop['stopCode'] . "' where ZID = '". $row['ZID'] ."'");
+                if (checkData($stop['stopCode'])) {
+                    $handle->exec("update ZTFCSTATIONMODEL set ZAPIKEY = 'gva', ZAPIID = '" . $stop['stopCode'] . "' where ZID = '" . $row['ZID'] . "'");
+                } else {
+                    print "No departures found for " . $row['ZNAME'] . "\n";
+                }
                 
             } else {
                 $parts = explode(",", $row['ZNAME']);
                 if (isset($parts[1])) {
                     if (trim($parts[1]) == $stop['stopName']) {
-                        $handle->exec("update ZTFCSTATIONMODEL set ZAPIKEY = 'gva', ZAPIID = '". $stop['stopCode'] . "' where ZID = '". $row['ZID'] ."'");
-                        print $parts[1] . ' == ' . $stop['stopName'] ."\n";
+                        if (checkData($stop['stopCode'])) {
+                            $handle->exec("update ZTFCSTATIONMODEL set ZAPIKEY = 'gva', ZAPIID = '". $stop['stopCode'] . "' where ZID = '". $row['ZID'] ."'");
+                            print $parts[1] . ' == ' . $stop['stopName'] ."\n";
+                        } else {
+                            print "No departures found for " . $row['ZNAME'] . "\n";
+                        }
                     }
                 } else {
                     print "Distance ($distance) too long for ". $row['ZNAME']. "\n";
@@ -48,4 +60,15 @@ function getFirst($url) {
     $result = file_get_contents( "http://www.timeforcoffee.ch/api/".$url);
     $r = json_decode($result, true);
     return $r['departures'];
+}
+
+function checkData($id)
+{
+    $url = "http://prod.ivtr-od.tpg.ch/v1/GetNextDepartures?key=21a25080-9bbc-11e4-bc99-0002a5d5c51b&stopCode=";
+    $json = file_get_contents($url . $id);
+    $r = json_decode($json, true);
+    if (isset($r['departures']) && count($r['departures']) > 0) {
+        return true;
+    }
+    return false;
 }
